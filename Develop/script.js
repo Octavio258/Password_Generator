cec201c705e71b710fa54d5f8e3a659bcbc33535
Octavio Politron <octavio.politron@gmail.com>
2022-01-18 20:37:48 -0800

    added special characters logic

--- Develop/script.js
+++ Develop/script.js
@@ -9,14 +9,30 @@ var characterPrompt = function () {
   }
 }
 
+var userPrompt = function() {
+  var SpecialChar = confirm ("Do you want special characters for your password?");
+
+  if (SpecialChar) {
+    passCharacters = passCharacters.concat(SpecialCharacters)
+    securePassword += generateRandomItems(SpecialCharacters)
+  }
+}
+
+var generateRandomItems = function(characterArray) {
+  console.log(characterArray);
+  guaranteeCharacters = characterArray[Math.floor(Math.random() * characterArray.characterLength)];
+  return guaranteeCharacters;
+}
+
 
 
 
 
 var numbers = ['0', '1','2','3','4','5','6','7','8','9'];
-var SpecialCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+var SpecialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 var Alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var CapitalAlphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var passCharacters = [];
 var securePassword = "";
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
